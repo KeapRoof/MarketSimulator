fb65f0a649043bd90a3d0e912d39989ffc4e38fd
Haithem <haithem.hadj-azzem@etu.univ-lyon1.fr>
2026-01-23 15:52:42 +0100

    Enhance Stock class to include dividend yield and market hours

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,8 +1,10 @@
 import com.market.assets.Stock;
 
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Market sim");
-        Stock asset = new Stock("Gold", "GLD", 1800.50);
+        Stock asset = new Stock("Gold", "GLD", 1800.50, 1.5f, LocalTime.of(9,0), LocalTime.of(17,30));
     }
 }
