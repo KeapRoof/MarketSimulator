cd4a85403b353c5e5c5ef6e7294c5f4e861b4ff3
Haithem <haithem.hadj-azzem@etu.univ-lyon1.fr>
2026-01-23 15:26:56 +0100

    Add AssetType to Asset class and create Stock subclass

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,8 +1,8 @@
-import com.market.assets.Asset;
+import com.market.assets.Stock;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Market sim");
-        Asset asset = new Asset("Gold", "GLD", 1800.50);
+        Stock asset = new Stock("Gold", "GLD", 1800.50);
     }
 }
