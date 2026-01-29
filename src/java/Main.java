9415024a6e7ed55c7eb2ebeda76b309538e4d32c
Haithem <haithem.hadj-azzem@etu.univ-lyon1.fr>
2026-01-29 14:00:08 +0100

    Add price fluctuation and updater to Asset class

--- src/java/Main.java
+++ src/java/Main.java
@@ -4,7 +4,13 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Market sim");
         Stock asset = new Stock("Gold", "GLD", 1800.50, 1.5f, LocalTime.of(9,0), LocalTime.of(17,30));
+        while(true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
     }
 }
